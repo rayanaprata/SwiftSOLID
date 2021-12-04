c101fe2cf0cda917bb534edc4bf810403f5c0cc4
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-12-03 22:39:48 -0300

    princípios do SOLID

---
+++ SwiftSOLID.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// O que é SOLID?
+
+// SOLID é um acrônimo dos cinco primeiros princípios da programação orientada a objetos e design de código identificados por Robert C. Martin (também conhecido como Uncle Bob) por volta do ano 2000. O acrônimo SOLID foi introduzido por Michael Feathers, após observar que os cinco princípios poderiam se encaixar nesta palavra.
+
+// S  SRP - Single Responsability Principle    (Princípio da responsabilidade única)
+// O  OCP - Open-Closed Principle              (Princípio Aberto-Fechado)
+// L  LSP - Liskov Substituition Principle     (Princípio de substituição do Liskov)
+// I  ISP - Interface Segregation Principle    (Princípio da Segregação da Interface)
+// D  DIP - Dependency Inversion Principle     (Princípio da inversão da dependência)
